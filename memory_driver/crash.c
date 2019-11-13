92bcafae41dbed91c3b4f3c784910071afc72ea8
Wang Long <w@laoqinren.net>
2019-11-13 14:39:43 +0800

    update memory_driver
    Signed-off-by: Wang Long <w@laoqinren.net>

--- memory_driver/crash.c
+++ memory_driver/crash.c
@@ -37,58 +37,6 @@
 
 extern int page_is_ram(unsigned long);
 
-#ifdef CONFIG_S390
-/*
- * For swapped prefix pages get bounce buffer using xlate_dev_mem_ptr()
- */
-static inline void *map_virtual(u64 offset, struct page **pp)
-{
-	struct page *page;
-	unsigned long pfn;
-	void *vaddr;
-
-	vaddr = xlate_dev_mem_ptr(offset);
-	pfn = ((unsigned long) vaddr) >> PAGE_SHIFT;
-	if ((unsigned long) vaddr != offset)
-		page = pfn_to_page(pfn);
-	else
-		page = NULL;
-
-	if (!page_is_ram(pfn)) {
-		printk(KERN_INFO
-		    "crash memory driver: !page_is_ram(pfn: %lx)\n", pfn);
-		return NULL;
-	}
-
-	if (!pfn_valid(pfn)) {
-		printk(KERN_INFO
-		    "crash memory driver: invalid pfn: %lx )\n", pfn);
-		return NULL;
-	}
-
-	*pp = page;
-	return vaddr;
-}
-
-/*
- * Free bounce buffer if necessary
- */
-static inline void unmap_virtual(struct page *page)
-{
-	void *vaddr;
-
-	if (page) {
-		/*
-		 * Because for bounce buffers vaddr will never be 0
-		 * unxlate_dev_mem_ptr() will always free the bounce buffer.
-		 */
-		vaddr = (void *)(page_to_pfn(page) << PAGE_SHIFT);
-		unxlate_dev_mem_ptr(0, vaddr);
-	}
-}
-
-#else  /* all architectures except s390x */
-
 static inline void *
 map_virtual(u64 offset, struct page **pp)
 {
@@ -132,10 +80,9 @@ map_virtual(u64 offset, struct page **pp)
 }
 
 static inline void unmap_virtual(struct page *page) 
-{ 
+{
 	kunmap(page);
 }
-#endif
 
 
 #define CRASH_VERSION   "1.4"
